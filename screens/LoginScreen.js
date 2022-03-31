1e25bc136afd20713c741212299a80b81280f916
ruli87 <ruli@opusgroup.dk>
2022-03-31 11:45:07 +0200

    stuff

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,25 @@
 import react from "react";
 import LoginForm from "../src/LoginForm";
+import { View, Text, StyleSheet } from "react-native"
 
 export default function LoginScreen(){
 
     return (
-        <LoginForm></LoginForm>
+            
+        <View style={styles.container}>
+            <Text>dsada</Text>
+            <LoginForm></LoginForm>
+        </View>
     );
-    
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "red", 
+        height: "100%",
+        width: "100%"
+    },
+});
+
+
+  
